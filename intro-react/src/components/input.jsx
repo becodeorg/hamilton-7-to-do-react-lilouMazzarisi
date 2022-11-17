8c3bf968f8b32b95b2bc962550ef38de6db9a3e9
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2022-11-17 21:46:53 +0100

    push 17112146

---
+++ intro-react/src/components/input.jsx
@@ -0,0 +1,10 @@
+const Input = () => {
+    return (
+        <form>
+            <input type="text" placeholder="Type New Todo"/>
+            <input type="submit" value="Add Todo" />
+        </form>
+    );
+}
+
+export default Input; 
