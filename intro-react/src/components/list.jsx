07f793e954efcdef7e4b0362d9aa7f78f264ab78
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2022-11-18 10:58:38 +0100

    added style

--- intro-react/src/components/list.jsx
+++ intro-react/src/components/list.jsx
@@ -2,16 +2,10 @@ const List = () => {
     return (
         <div className="ml-8 pt-8">
             <h3 className="text-left text-2xl font-bold">Todos</h3>
-            <div className="listing">
-                <div className="flex items-center">
-                    <input type="checkbox"/>
-                    <p className="pl-2">Learn React</p>
-                </div>
-                <div className="flex items-center">
-                    <input type="checkbox"/>
-                    <p className="pl-2">Be Awesome!</p>
-                </div>
-            </div>  
+            <ul>
+                <li><input type="checkbox"/> Learn React</li>
+                <li><input type="checkbox"/> Be Awesome!</li>
+            </ul>
         </div>
     );
 }
