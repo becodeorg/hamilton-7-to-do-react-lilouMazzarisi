3385e75d55827a9224b24d537874ef1ebc3d9f31
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2022-11-18 10:34:05 +0100

    added style

--- intro-react/src/components/input.jsx
+++ intro-react/src/components/input.jsx
@@ -2,7 +2,7 @@ const Input = () => {
     return (
         <form className="todoInput line flex flex-col ml-8 pt-8 pb-5">
             <input className="bg-gray-100 border border-solid border-gray-300 rounded-md w-max p-1" type="text" placeholder="Type New Todo"/>
-            <input className="bg-blue-500 text-white rounded-md w-24 p-1 mt-2" type="submit" value="Add Todo" />
+            <input className="bg-blue-500 text-white rounded-md w-24 p-1 mt-2 cursor-pointer" type="submit" value="Add Todo" />
         </form>
     );
 }
