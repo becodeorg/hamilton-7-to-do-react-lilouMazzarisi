c051d4f8ad6f9b312b12a7a537546ae79a78eaca
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2022-11-18 16:09:23 +0100

    chapter 2 upload

--- intro-react/src/components/list.jsx
+++ intro-react/src/components/list.jsx
@@ -1,13 +1,35 @@
-const List = () => {
+import React, { useState } from 'react'
+
+function ListItem(item) {
+    const [unchecked, setundChecked] = useState(false);
+    
+
+    return ( <li className={unchecked ? "line-through" : ""}>
+                <input
+                    type="checkbox"
+                    defaultChecked={unchecked}
+                    onChange={() => setundChecked(!unchecked)}
+                />
+                {item.value}
+            </li> 
+    ); 
+
+}
+
+export default function List() {
+    const Todos = ["My firts todo", "My second todo"];
+    const [todos, setTodos] = useState(Todos);  
+
+    const listItems = Todos.map((todo)=> (
+        <ListItem key={todo.toString()} value={todo}/>
+    ))
+
     return (
         <div className="ml-8 pt-8">
             <h3 className="text-left text-2xl font-bold">Todos</h3>
             <ul>
-                <li><input type="checkbox"/> Learn React</li>
-                <li><input type="checkbox"/> Be Awesome!</li>
+                {listItems}
             </ul>
         </div>
     );
 }
-
-export default List; 
