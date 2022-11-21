abe881da1e6ef5f50b96c51a57ac977dc438c53b
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2022-11-21 11:23:18 +0100

    push chapter 2

--- intro-react/src/components/list.jsx
+++ intro-react/src/components/list.jsx
@@ -1,27 +1,26 @@
 import React, { useState } from 'react'
 
 function ListItem(item) {
-    const [unchecked, setundChecked] = useState(false);
-    
+    const [unchecked, setUnchecked] = useState(false);
 
-    return ( <li className={unchecked ? "line-through" : ""}>
-                <input
-                    type="checkbox"
-                    defaultChecked={unchecked}
-                    onChange={() => setundChecked(!unchecked)}
-                />
-                {item.value}
-            </li> 
-    ); 
+    return (<li className={unchecked ? "line-through" : ""}>
+        <input
+            type="checkbox"
+            defaultChecked={unchecked}
+            onChange={() => setUnchecked(!unchecked)}
+        />
+        {item.value}
+    </li>
+    );
 
 }
 
 export default function List() {
-    const Todos = ["My firts todo", "My second todo"];
-    const [todos, setTodos] = useState(Todos);  
+    const Todos = ["My first todo", "My second todo", "Another todo", "Yet another todo", "todo", "to the loo"];
+    const [todos, setTodos] = useState(Todos);
 
-    const listItems = Todos.map((todo)=> (
-        <ListItem key={todo.toString()} value={todo}/>
+    const listItems = Todos.map((todo) => (
+        <ListItem key={todo.toString()} value={todo} />
     ))
 
     return (
